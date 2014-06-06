fb6d24f1b3e6e7443319ca2a578f866d0a920dea
rbdev <rbdev@inbox.com>
2014-06-06 03:12:41 -0700

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
-    # @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User",
+                     email: "user@example.com", 
+                     password: "foobar", 
+                     password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -37,7 +38,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com double@period..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -99,31 +100,30 @@ describe User do
     end
   end
 
-  # # HACK, DEBUG.
+  # HACK, DEBUG.
+  # From tut.
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+  # # User custom, prob. better to do away with regular expressions in favor of eq.
   # describe "when email contains upper case letters" do
-  #   it "should be invalid" do
-  #       @user.email = 'EMAIL_ADDRESS_WITH_UPPERCASE_LETTERS@example.com'
-  #       expect(@user).not_to be_valid
+  #   before do
+  #     @user.email = @user.email.upcase
+  #     @user.save
+  #   end
+
+  #   it "should be turned to lower case letters" do
+  #     @user.reload
+  #     @user.email.should_not =~ /[A-Z]/x
   #   end
   # end
 
 end
 
-
-# require 'spec_helper'
-
-# describe User do
-
-#   before { @user = User.new(name: "Example User", email: "user@example.com") }
-
-#   subject { @user }
-
-#   it { should respond_to(:name) }
-#   it { should respond_to(:email) }
-# end
-
-# require 'spec_helper'
-
-# describe User do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
